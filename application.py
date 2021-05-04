0013100b6160b3f46db056884490079e9359aa7f
Yeabsira <Yeabsiram@outlook.com>
2021-05-04 17:27:04 +0300

    basics done,UI fix on index and chat in full screen remain

--- application.py
+++ application.py
@@ -1,26 +1,94 @@
-from flask import Flask, session, render_template
+import os, hashlib
+
+from flask import Flask, session, render_template,request, redirect,url_for, g
+from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from flask_socketio import SocketIO, send
 from form import *
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='7ff61fae7049489'
+socketio = SocketIO(app, cors_allowed_origins='*')
 
-# @app.before_request
-# def before_request():
-#     g.user = None
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
 
-#     if 'userName' in session:
-#         g.user = session['userName']
+@app.before_request
+def before_request():
+    g.user = None
+
+    if 'userName' in session:
+        g.user = db.execute(f"SELECT * FROM users WHERE username = '{session['userName']}'").first().username
 
 @app.route("/")
 def index():
-    userName="y"
-    return render_template('index.html',user=userName)
-
-@app.route("/login")
-def login():
-    return render_template('login.html',form=LoginForm())
-
-@app.route("/signup")
-def signup():
-    return render_template('signup.html',form=SignupForm())
+    if g.user is None:
     
+        return redirect(url_for('login'))
+    return render_template('index.html',user=g.user)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if request.method == 'POST':
+
+        session.pop('user_id', None)
+        username = form.username.data
+        password = hashlib.sha1(form.password.data.encode('utf-8')).hexdigest()
+
+        user = db.execute(f"SELECT * FROM users WHERE username='{username}' AND password='{password}'").first()
+
+        if user is None:
+
+            return render_template("login.html",message="* Wrong password or username", form=form)
+
+        if user:
+
+            session['userName'] = user.username
+            return redirect(url_for('index'))
+
+    return render_template("login.html", form=form)
+
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+    form = SignupForm()
+
+    if request.method == "POST":
+
+        username = form.username.data
+        password = hashlib.sha1(form.password.data.encode('utf-8')).hexdigest()
+        email = form.email.data
+
+        if db.execute("SELECT * FROM users WHERE username = :username", {"username": username}).rowcount != 0:
+            return render_template("signup.html",message="* Username "+username+ " is already in use",form=form)
+        
+        if db.execute("SELECT * FROM users WHERE email = :email", {"email": email}).rowcount != 0:
+            return render_template("signup.html",message="* Email is already in use",form=form)
+
+        db.execute(f"INSERT INTO users(username, email, password) VALUES ('{username}', '{email}', '{password}');")
+        db.commit()
+
+        session['userName'] = username
+
+        return redirect(url_for('index'))
+
+
+    return render_template("signup.html", form=form)
+
+@app.route("/logout")
+def logout():
+
+    session.pop('userName', None)
+    g.user = None
+
+    return redirect(url_for('login'))
+
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    socketio.emit('my response', json)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
---
+++ static/js/messageHandler.js
@@ -0,0 +1,27 @@
+var socket = io.connect('http://' + document.domain + ':' + location.port);
+
+socket.on( 'connect', function() {
+  socket.emit( 'my event', {
+    data: 'User Connected'
+  } )
+  var form = $( 'form' ).on( 'submit', function( e ) {
+    e.preventDefault()
+    let user_name = $( '.user_name' ).val()
+    let user_input = $( '.chat_input' ).val()
+    socket.emit( 'my event', {
+      user_name : user_name,
+      message : user_input
+    } )
+    $( '.chat_input' ).val( '' ).focus()
+  } )
+} )
+socket.on( 'my response', function( msg ) {
+  console.log( msg )
+  if( typeof msg.user_name !== 'undefined' ) {
+    var now = new Date(Date.now());
+    var formatted = now.getHours() + ":" + now.getMinutes();
+    $( '#messages' ).append( `  <p class="mb-3">${formatted} <b class="username">${msg.user_name}</b> <i class="message">${msg.message}</i></p>
+    <hr>` )
+   
+  }
+})
